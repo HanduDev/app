c5b40105510aa61953bd26965619e795a6b5ea98
YanGomes7 <yan.almeida@sou.unifeob.edu.br>
2025-03-24 21:36:51 -0300

    HND-45 Adiciona login com o google na tela de login

--- lib/ui/login/widgets/login_page.dart
+++ lib/ui/login/widgets/login_page.dart
@@ -1,3 +1,7 @@
+import 'package:app/helpers/errors.dart';
+import 'package:app/helpers/toast.dart';
+import 'package:app/providers/auth_provider.dart';
+import 'package:app/routes/routes.dart';
 import 'package:app/ui/core/shared/flat_button.dart';
 import 'package:app/ui/core/shared/primary_button.dart';
 import 'package:app/ui/core/shared/text_input.dart';
@@ -5,78 +9,96 @@ import 'package:app/ui/core/themes/app_colors.dart';
 import 'package:app/ui/core/themes/font.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/Logo_1.png', height: 100),
-            SizedBox(height: 24),
-            Text(
-              'Login',
-              style: Font.primary(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: AppColors.primary500),
-            ),
-            SizedBox(height: 48),
-            TextInput(
-              label: 'Email',
-            ),
-            SizedBox(height: 56),
-            TextInput(
-              label: 'Senha',
-              obscureText: true,
-            ),
-            SizedBox(height: 56),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Consumer<AuthProvider>(
+        builder: (context, authProvider, child) {
+          void onLoginWithGoogle() async {
+            try {
+              await authProvider.signInWithGoogle();
+
+              if (context.mounted) {
+                context.pushReplacement(Routes.home);
+              }
+            } catch (e) {
+              if (!context.mounted) return;
+              Toast.error(context, getErrorMessage(e));
+            }
+          }
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Image.asset('assets/images/Logo_1.png', height: 100),
+                SizedBox(height: 24),
+                Text(
+                  'Login',
+                  style: Font.primary(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.primary500,
+                  ),
+                ),
+                SizedBox(height: 48),
+                TextInput(label: 'Email'),
+                SizedBox(height: 56),
+                TextInput(label: 'Senha', obscureText: true),
+                SizedBox(height: 56),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Checkbox(value: false, onChanged: (value) {}),
-                    Text(
-                      'Lembrar-Me',
-                      style: Font.primary(
-                          fontSize: 12, color: AppColors.primary500),
+                    Row(
+                      children: [
+                        Checkbox(value: false, onChanged: (value) {}),
+                        Text(
+                          'Lembrar-Me',
+                          style: Font.primary(
+                            fontSize: 12,
+                            color: AppColors.primary500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Esqueceu a senha?',
+                        style: Font.primary(
+                          fontSize: 12,
+                          color: AppColors.primary500,
+                        ),
+                      ),
                     ),
                   ],
                 ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Esqueceu a senha?',
-                    style:
-                        Font.primary(fontSize: 12, color: AppColors.primary500),
+                SizedBox(height: 88),
+                PrimaryButton(text: 'Entrar', onPressed: () {}),
+                SizedBox(height: 56),
+                Text('Ou continue com'),
+                SizedBox(height: 24),
+                FlatButton(
+                  loading: authProvider.isGoogleLoading,
+                  onPressed: onLoginWithGoogle,
+                  leftIcon: SvgPicture.asset(
+                    'assets/images/icons/google.svg',
+                    height: 30,
+                    width: 30,
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 88),
-            PrimaryButton(
-              text: 'Entrar',
-              onPressed: () {},
-            ),
-            SizedBox(height: 56),
-            Text('Ou continue com'),
-            SizedBox(height: 24),
-            FlatButton(
-              loading: authProvider.isGoogleLoading,
-              onPressed: onLoginWithGoogle,
-              text: 'Entrar com o Google',
-              leftIcon: SvgPicture.asset(
-                'assets/images/icons/google.svg',
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }

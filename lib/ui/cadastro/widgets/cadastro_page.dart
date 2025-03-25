24f13b67c206623ff3436b11d7d82c09a5acb031
faKonda <f_konda@yahoo.com>
2025-03-24 21:31:54 -0300

    HND-44 Corrige os paddings da tela de cadastro, adiciona login com o google e opção de mostrar e esconder a senha.

--- lib/ui/cadastro/widgets/cadastro_page.dart
+++ lib/ui/cadastro/widgets/cadastro_page.dart
@@ -1,3 +1,6 @@
+import 'package:app/helpers/errors.dart';
+import 'package:app/helpers/toast.dart';
+import 'package:app/providers/auth_provider.dart';
 import 'package:app/ui/core/themes/app_colors.dart';
 import 'package:app/ui/core/themes/font.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +10,7 @@ import 'package:app/routes/routes.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:app/ui/core/shared/flat_button.dart';
+import 'package:provider/provider.dart';
 
 class CadastroPage extends StatefulWidget {
   const CadastroPage({super.key});
@@ -26,133 +30,86 @@ class _CadastroPageState extends State<CadastroPage> {
         backgroundColor: AppColors.white,
         body: Center(
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+            child: Consumer<AuthProvider>(
+              builder: (context, authProvider, child) {
+                void onLoginWithGoogle() async {
+                  try {
+                    await authProvider.signInWithGoogle();
 
-              children: [
-                const SizedBox(height: 40,),
-                Image.asset('assets/images/Logo.png', width: 153, height: 147),// Trocar imagem por uma maior
-                const SizedBox(height: 10),
-                Text('Cadastro',
-                  style: Font.primary (
-                    color: AppColors.grey,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold
-                  )
-                ),
-                const SizedBox(height: 40),
+                    if (context.mounted) {
+                      context.pushReplacement(Routes.home);
+                    }
+                  } catch (e) {
+                    if (!context.mounted) return;
+                    Toast.error(context, getErrorMessage(e));
+                  }
+                }
 
-                Column(
-                  children: [
-                    Padding(padding: const EdgeInsets.symmetric(horizontal: 40.00),
-                    child:
-                    TextInput(
-                      label: 'Nome Completo',
-                  ),
-                  ),
-                    const SizedBox(height: 30,),
-                    Padding(padding: const EdgeInsets.symmetric(horizontal: 40.00),
-                      child:
-                      TextInput(
-                        label: 'Email',
-                    ),
-                    ),
-                    const SizedBox(height: 30,),
-                    Padding(padding: const EdgeInsets.symmetric(horizontal: 40.00),
-                      child:
-                      TextInput(
-                        label: 'Senha',
-                        obscureText: true,
-                    ),
-                    ),
-                    const SizedBox(height: 10,),
-                    Padding(padding: const EdgeInsets.only(left: 40.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text('Mínimo 8 dígitos',
-                          style: Font.primary(
-                            fontSize: 12,
-                            color: AppColors.primary100,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 40),
+                      Image.asset(
+                        'assets/images/Logo_1.png',
+                        width: 153,
+                        height: 147,
                       ),
-                    ),
-                    const SizedBox(height: 5,),
-                    Padding(padding: const EdgeInsets.only(left: 40.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text('Utilizar letras e números',
-                          style: Font.primary(
-                            fontSize: 12,
-                            color: AppColors.primary100,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    ],
-                ),
-                const SizedBox(height: 20,),
-                    Padding(padding: const EdgeInsets.symmetric(horizontal: 40.00),
-                      child:
-                      TextInput(
-                        label: 'Confirmar Senha',
-                        obscureText: true,
-                    ),
-                    ),
-                const SizedBox(height: 40,),
-                    Padding(padding: const EdgeInsets.symmetric(horizontal: 40.00),
-                      child: PrimaryButton(
-                        rounded: true,
-                        onPressed: () {
-                        context.pushReplacement(
-                        Routes.confirmacaoCadastro,
-                      );
-                    },
-                    text: 'Cadastrar conta',
-                      ),
-                    ),
-                const SizedBox(height: 20,),
-                    Text('Ou continue com',
-                    style: Font.primary(
-                      fontSize: 12
-                    ),),
-                const SizedBox(height: 32),
-                    FlatButton(
-                      //loading: authProvider.isGoogleLoading,
-                      //onPressed: onLoginWithGoogle,
-                      leftIcon: SvgPicture.asset(
-                      'assets/images/icons/google.svg',
-                      height: 30,
-                      width: 30,
-                    ),
-                  ),
-                const SizedBox(height: 10,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Já tem uma conta?',
-                          style: Font.primary(
-                            fontSize: 12,
-                        ),
-                      ),
-                     TextButton(
-                      onPressed: (){
-                        context.pushReplacement(
-                          Routes.login //Trocar para login quando tiver
-                        );
-                      },
-                      child: Text('Entrar',
+                      const SizedBox(height: 10),
+                      Text(
+                        'Cadastro',
                         style: Font.primary(
-                          fontSize: 12,
-                          color: AppColors.primary300
-                        ),)
-                      )
-
-                   ],
-                 )
-              ],
+                          color: AppColors.grey,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      FormsValidator(),
+                      const SizedBox(height: 40),
+                      Text(
+                        'Ou continue com',
+                        style: Font.primary(fontSize: 12),
+                      ),
+                      const SizedBox(height: 32),
+                      FlatButton(
+                        loading: authProvider.isGoogleLoading,
+                        onPressed: onLoginWithGoogle,
+                        leftIcon: SvgPicture.asset(
+                          'assets/images/icons/google.svg',
+                          height: 30,
+                          width: 30,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Já tem uma conta?',
+                            style: Font.primary(fontSize: 12),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              context.pushReplacement(
+                                Routes.login, //Trocar para login quando tiver
+                              );
+                            },
+                            child: Text(
+                              'Entrar',
+                              style: Font.primary(
+                                fontSize: 12,
+                                color: AppColors.primary300,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
           ),
         ),
@@ -162,7 +119,7 @@ class _CadastroPageState extends State<CadastroPage> {
 }
 
 class FormsValidator extends StatefulWidget {
-  const FormsValidator ({super.key});
+  const FormsValidator({super.key});
 
   @override
   FormsValidatorState createState() {
@@ -172,6 +129,9 @@ class FormsValidator extends StatefulWidget {
 
 class FormsValidatorState extends State<FormsValidator> {
   final _formKey = GlobalKey<FormState>();
+
+  bool _obscurePasswordText = true;
+  bool _obscureConfirmPasswordText = true;
 
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
@@ -191,121 +151,110 @@ class FormsValidatorState extends State<FormsValidator> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40.0),
-            child: TextInput(
-              controller: _nameController,
-              label: 'Nome Completo',
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Digite um Nome';
-                }
-                return null;
-              },
-            ),
+          TextInput(
+            controller: _nameController,
+            label: 'Nome Completo',
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Digite um Nome';
+              }
+              return null;
+            },
           ),
           const SizedBox(height: 30),
 
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40.0),
-            child: TextInput(
-              controller: _emailController,
-              label: 'Email',
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Digite um e-mail';
-                }
-                if (!RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$') // Validar .dominio corretamente
-                    .hasMatch(value)) {
-                  return 'Digite um e-mail válido';
-                }
-                return null;
-              },
-            ),
+          TextInput(
+            controller: _emailController,
+            label: 'Email',
+            keyboardType: TextInputType.emailAddress,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Digite um e-mail';
+              }
+              if (!RegExp(
+                r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+              ) // Validar .dominio corretamente
+              .hasMatch(value)) {
+                return 'Digite um e-mail válido';
+              }
+              return null;
+            },
           ),
           const SizedBox(height: 30),
 
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40.0),
-            child: TextInput(
-              controller: _passwordController,
-              label: 'Senha',
-              obscureText: true,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Digite uma senha';
-                }
-                if (value.length < 8) {
-                  return 'A senha deve ter pelo menos 8 caracteres';
-                }
-                if (!RegExp(r'^(?=.*[a-zA-Z])(?=.*\d)').hasMatch(value)) {
-                  return 'A senha deve conter letras e números';
-                }
-                return null;
+          TextInput(
+            controller: _passwordController,
+            label: 'Senha',
+            obscureText: _obscurePasswordText,
+            sufixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  _obscurePasswordText = !_obscurePasswordText;
+                });
               },
-            ),
-          ),
-          const SizedBox(height: 10),
-
-          Padding(
-            padding: const EdgeInsets.only(left: 40.0),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Mínimo 8 dígitos',
-                style: Font.primary(
-                  fontSize: 12,
-                  color: AppColors.primary100,
-                  fontWeight: FontWeight.bold,
-                ),
+              icon: Icon(
+                _obscurePasswordText ? Icons.visibility : Icons.visibility_off,
               ),
             ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Digite uma senha';
+              }
+              if (value.length < 8) {
+                return 'A senha deve ter pelo menos 8 caracteres';
+              }
+              if (!RegExp(r'^(?=.*[a-zA-Z])(?=.*\d)').hasMatch(value)) {
+                return 'A senha deve conter letras e números';
+              }
+              return null;
+            },
           ),
-          const SizedBox(height: 5),
-
-          Padding(
-            padding: const EdgeInsets.only(left: 40.0),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Utilizar letras e números',
-                style: Font.primary(
-                  fontSize: 12,
-                  color: AppColors.primary100,
-                  fontWeight: FontWeight.bold,
-                ),
+          const SizedBox(height: 10),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Utilizar letras e números',
+              style: Font.primary(
+                fontSize: 12,
+                color: AppColors.primary100,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
           const SizedBox(height: 20),
 
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40.0),
-            child: TextInput(
-              controller: _password2Controller,
-              label: 'Confirmar Senha',
-              obscureText: true,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Confirme sua senha';
-                }
-                if (value != _passwordController.text) {
-                  return 'As senhas não são iguais';
-                }
-                return null;
+          TextInput(
+            controller: _password2Controller,
+            label: 'Confirmar Senha',
+            obscureText: _obscureConfirmPasswordText,
+            sufixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  _obscureConfirmPasswordText = !_obscureConfirmPasswordText;
+                });
               },
+              icon: Icon(
+                _obscureConfirmPasswordText
+                    ? Icons.visibility
+                    : Icons.visibility_off,
+              ),
             ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Confirme sua senha';
+              }
+              if (value != _passwordController.text) {
+                return 'As senhas não são iguais';
+              }
+              return null;
+            },
           ),
           const SizedBox(height: 40),
 
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40.0),
-            child: PrimaryButton(
-              text: ('Cadastrar conta'),
-              rounded: true,
-              onPressed: _validatedAndSubmit,
-            )
+          PrimaryButton(
+            text: ('Cadastrar conta'),
+            rounded: true,
+            onPressed: _validatedAndSubmit,
           ),
         ],
       ),

fe7e5ea2bf84ef5ed97bf479e63b6505f6530637
LucasPizol <lucaspizolfe@gmail.com>
2025-04-16 20:33:56 -0300

    HND-64 Remove pacotes não utilizados

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,4 +1,3 @@
-import 'package:app/data/repositories/trail/trail_repository.dart';
 import 'package:app/providers/auth_provider.dart';
 import 'package:app/routes/branches/account_branches.dart';
 import 'package:app/routes/branches/auth_branches.dart';
@@ -7,10 +6,8 @@ import 'package:app/routes/branches/education_branches.dart';
 import 'package:app/routes/branches/libras_branches.dart';
 import 'package:app/routes/branches/translate_branches.dart';
 import 'package:app/ui/core/shared/common_layout.dart';
-import 'package:app/ui/educacao/plano_de_estudos/view_model/forms_container_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 import 'routes.dart';
 
 GoRouter router(AuthProvider authProvider) => GoRouter(

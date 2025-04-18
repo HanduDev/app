14f92901f4d381b8d4ffa10b24af52d8dbf22b6c
Unity <guilhermtrout@gmail.com>
2025-04-17 22:04:32 -0300

    HND-62 [Test]  Criação dos Testes Unitarios tela de login

--- test/ui/login/login_page_test.dart
+++ test/ui/login/login_page_test.dart
@@ -1,9 +1,80 @@
-import 'package:mockito/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
 import 'package:app/providers/auth_provider.dart';
+import 'package:app/ui/login/widgets/login_controller.dart';
 
+import 'login_page_test.mocks.dart';
 
-@GenerateMocks([AuthProvider, BuildContext])
+@GenerateMocks([AuthProvider])
 void main() {
-  
+  late MockAuthProvider mockAuthProvider;
+
+  setUp(() {
+    mockAuthProvider = MockAuthProvider();
+  });
+
+  testWidgets('Deve fazer login com sucesso', (WidgetTester tester) async {
+    when(
+      mockAuthProvider.signInWithGoogle(),
+    ).thenAnswer((_) async => Future.value());
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Builder(
+          builder: (context) {
+            return Scaffold(
+              body: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    LoginController.loginWithGoogle(context, mockAuthProvider);
+                  },
+                  child: const Text('Login'),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+
+    await tester.tap(find.text('Login'));
+    await tester.pumpAndSettle();
+
+    verify(mockAuthProvider.signInWithGoogle()).called(1);
+  });
+
+  testWidgets('Deve exibir Toast de erro se ocorrer uma exceção no login', (
+    WidgetTester tester,
+  ) async {
+    when(
+      mockAuthProvider.signInWithGoogle(),
+    ).thenThrow(Exception('Erro no Google'));
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Builder(
+          builder: (context) {
+            return Scaffold(
+              body: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    LoginController.loginWithGoogle(context, mockAuthProvider);
+                  },
+                  child: const Text('Login'),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+
+    await tester.tap(find.text('Login'));
+    await tester.pump();
+
+    expect(find.byType(SnackBar), findsOneWidget);
+    expect(find.textContaining('Erro'), findsOneWidget);
+  });
 }
--- test/ui/login/login_page_test.mocks.dart
+++ test/ui/login/login_page_test.mocks.dart
@@ -3,14 +3,22 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i5;
-import 'dart:ui' as _i6;
+import 'dart:async' as _i10;
+import 'dart:ui' as _i11;
 
-import 'package:app/providers/auth_provider.dart' as _i4;
+import 'package:app/providers/auth_provider.dart' as _i9;
 import 'package:flutter/material.dart' as _i2;
-import 'package:flutter/src/foundation/assertions.dart' as _i7;
+import 'package:flutter/src/foundation/assertions.dart' as _i12;
 import 'package:flutter/src/foundation/diagnostics.dart' as _i3;
+import 'package:go_router/src/configuration.dart' as _i4;
+import 'package:go_router/src/delegate.dart' as _i5;
+import 'package:go_router/src/information_provider.dart' as _i6;
+import 'package:go_router/src/match.dart' as _i15;
+import 'package:go_router/src/parser.dart' as _i7;
+import 'package:go_router/src/router.dart' as _i13;
+import 'package:go_router/src/state.dart' as _i8;
 import 'package:mockito/mockito.dart' as _i1;
+import 'package:mockito/src/dummies.dart' as _i14;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -57,10 +65,45 @@ class _FakeDiagnosticsNode_2 extends _i1.SmartFake
   }) => super.toString();
 }
 
+class _FakeRouteConfiguration_3 extends _i1.SmartFake
+    implements _i4.RouteConfiguration {
+  _FakeRouteConfiguration_3(Object parent, Invocation parentInvocation)
+    : super(parent, parentInvocation);
+}
+
+class _FakeBackButtonDispatcher_4 extends _i1.SmartFake
+    implements _i2.BackButtonDispatcher {
+  _FakeBackButtonDispatcher_4(Object parent, Invocation parentInvocation)
+    : super(parent, parentInvocation);
+}
+
+class _FakeGoRouterDelegate_5 extends _i1.SmartFake
+    implements _i5.GoRouterDelegate {
+  _FakeGoRouterDelegate_5(Object parent, Invocation parentInvocation)
+    : super(parent, parentInvocation);
+}
+
+class _FakeGoRouteInformationProvider_6 extends _i1.SmartFake
+    implements _i6.GoRouteInformationProvider {
+  _FakeGoRouteInformationProvider_6(Object parent, Invocation parentInvocation)
+    : super(parent, parentInvocation);
+}
+
+class _FakeGoRouteInformationParser_7 extends _i1.SmartFake
+    implements _i7.GoRouteInformationParser {
+  _FakeGoRouteInformationParser_7(Object parent, Invocation parentInvocation)
+    : super(parent, parentInvocation);
+}
+
+class _FakeGoRouterState_8 extends _i1.SmartFake implements _i8.GoRouterState {
+  _FakeGoRouterState_8(Object parent, Invocation parentInvocation)
+    : super(parent, parentInvocation);
+}
+
 /// A class which mocks [AuthProvider].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockAuthProvider extends _i1.Mock implements _i4.AuthProvider {
+class MockAuthProvider extends _i1.Mock implements _i9.AuthProvider {
   MockAuthProvider() {
     _i1.throwOnMissingStub(this);
   }
@@ -100,34 +143,34 @@ class MockAuthProvider extends _i1.Mock implements _i4.AuthProvider {
           as bool);
 
   @override
-  _i5.Future<void> init() =>
+  _i10.Future<void> init() =>
       (super.noSuchMethod(
             Invocation.method(#init, []),
-            returnValue: _i5.Future<void>.value(),
-            returnValueForMissingStub: _i5.Future<void>.value(),
+            returnValue: _i10.Future<void>.value(),
+            returnValueForMissingStub: _i10.Future<void>.value(),
           )
-          as _i5.Future<void>);
+          as _i10.Future<void>);
 
   @override
-  _i5.Future<void> signInWithGoogle() =>
+  _i10.Future<void> signInWithGoogle() =>
       (super.noSuchMethod(
             Invocation.method(#signInWithGoogle, []),
-            returnValue: _i5.Future<void>.value(),
-            returnValueForMissingStub: _i5.Future<void>.value(),
+            returnValue: _i10.Future<void>.value(),
+            returnValueForMissingStub: _i10.Future<void>.value(),
           )
-          as _i5.Future<void>);
+          as _i10.Future<void>);
 
   @override
-  _i5.Future<void> signOut() =>
+  _i10.Future<void> signOut() =>
       (super.noSuchMethod(
             Invocation.method(#signOut, []),
-            returnValue: _i5.Future<void>.value(),
-            returnValueForMissingStub: _i5.Future<void>.value(),
+            returnValue: _i10.Future<void>.value(),
+            returnValueForMissingStub: _i10.Future<void>.value(),
           )
-          as _i5.Future<void>);
+          as _i10.Future<void>);
 
   @override
-  _i5.Future<void> signIn({
+  _i10.Future<void> signIn({
     required String? email,
     required String? password,
   }) =>
@@ -136,13 +179,13 @@ class MockAuthProvider extends _i1.Mock implements _i4.AuthProvider {
               #email: email,
               #password: password,
             }),
-            returnValue: _i5.Future<void>.value(),
-            returnValueForMissingStub: _i5.Future<void>.value(),
+            returnValue: _i10.Future<void>.value(),
+            returnValueForMissingStub: _i10.Future<void>.value(),
           )
-          as _i5.Future<void>);
+          as _i10.Future<void>);
 
   @override
-  _i5.Future<void> signUp({
+  _i10.Future<void> signUp({
     required String? name,
     required String? email,
     required String? password,
@@ -153,37 +196,37 @@ class MockAuthProvider extends _i1.Mock implements _i4.AuthProvider {
               #email: email,
               #password: password,
             }),
-            returnValue: _i5.Future<void>.value(),
-            returnValueForMissingStub: _i5.Future<void>.value(),
+            returnValue: _i10.Future<void>.value(),
+            returnValueForMissingStub: _i10.Future<void>.value(),
           )
-          as _i5.Future<void>);
+          as _i10.Future<void>);
 
   @override
-  _i5.Future<void> verifyCode({required String? code}) =>
+  _i10.Future<void> verifyCode({required String? code}) =>
       (super.noSuchMethod(
             Invocation.method(#verifyCode, [], {#code: code}),
-            returnValue: _i5.Future<void>.value(),
-            returnValueForMissingStub: _i5.Future<void>.value(),
+            returnValue: _i10.Future<void>.value(),
+            returnValueForMissingStub: _i10.Future<void>.value(),
           )
-          as _i5.Future<void>);
+          as _i10.Future<void>);
 
   @override
-  _i5.Future<void> resendCode({required String? code}) =>
+  _i10.Future<void> resendCode({required String? code}) =>
       (super.noSuchMethod(
             Invocation.method(#resendCode, [], {#code: code}),
-            returnValue: _i5.Future<void>.value(),
-            returnValueForMissingStub: _i5.Future<void>.value(),
+            returnValue: _i10.Future<void>.value(),
+            returnValueForMissingStub: _i10.Future<void>.value(),
           )
-          as _i5.Future<void>);
+          as _i10.Future<void>);
 
   @override
-  void addListener(_i6.VoidCallback? listener) => super.noSuchMethod(
+  void addListener(_i11.VoidCallback? listener) => super.noSuchMethod(
     Invocation.method(#addListener, [listener]),
     returnValueForMissingStub: null,
   );
 
   @override
-  void removeListener(_i6.VoidCallback? listener) => super.noSuchMethod(
+  void removeListener(_i11.VoidCallback? listener) => super.noSuchMethod(
     Invocation.method(#removeListener, [listener]),
     returnValueForMissingStub: null,
   );
@@ -275,7 +318,7 @@ class MockBuildContext extends _i1.Mock implements _i2.BuildContext {
   @override
   _i2.DiagnosticsNode describeElement(
     String? name, {
-    _i7.DiagnosticsTreeStyle? style = _i7.DiagnosticsTreeStyle.errorProperty,
+    _i12.DiagnosticsTreeStyle? style = _i12.DiagnosticsTreeStyle.errorProperty,
   }) =>
       (super.noSuchMethod(
             Invocation.method(#describeElement, [name], {#style: style}),
@@ -289,7 +332,7 @@ class MockBuildContext extends _i1.Mock implements _i2.BuildContext {
   @override
   _i2.DiagnosticsNode describeWidget(
     String? name, {
-    _i7.DiagnosticsTreeStyle? style = _i7.DiagnosticsTreeStyle.errorProperty,
+    _i12.DiagnosticsTreeStyle? style = _i12.DiagnosticsTreeStyle.errorProperty,
   }) =>
       (super.noSuchMethod(
             Invocation.method(#describeWidget, [name], {#style: style}),
@@ -322,4 +365,292 @@ class MockBuildContext extends _i1.Mock implements _i2.BuildContext {
             ),
           )
           as _i2.DiagnosticsNode);
+}
+
+/// A class which mocks [GoRouter].
+///
+/// See the documentation for Mockito's code generation for more information.
+class MockGoRouter extends _i1.Mock implements _i13.GoRouter {
+  MockGoRouter() {
+    _i1.throwOnMissingStub(this);
+  }
+
+  @override
+  _i4.RouteConfiguration get configuration =>
+      (super.noSuchMethod(
+            Invocation.getter(#configuration),
+            returnValue: _FakeRouteConfiguration_3(
+              this,
+              Invocation.getter(#configuration),
+            ),
+          )
+          as _i4.RouteConfiguration);
+
+  @override
+  set configuration(_i4.RouteConfiguration? _configuration) =>
+      super.noSuchMethod(
+        Invocation.setter(#configuration, _configuration),
+        returnValueForMissingStub: null,
+      );
+
+  @override
+  _i2.BackButtonDispatcher get backButtonDispatcher =>
+      (super.noSuchMethod(
+            Invocation.getter(#backButtonDispatcher),
+            returnValue: _FakeBackButtonDispatcher_4(
+              this,
+              Invocation.getter(#backButtonDispatcher),
+            ),
+          )
+          as _i2.BackButtonDispatcher);
+
+  @override
+  _i5.GoRouterDelegate get routerDelegate =>
+      (super.noSuchMethod(
+            Invocation.getter(#routerDelegate),
+            returnValue: _FakeGoRouterDelegate_5(
+              this,
+              Invocation.getter(#routerDelegate),
+            ),
+          )
+          as _i5.GoRouterDelegate);
+
+  @override
+  set routerDelegate(_i5.GoRouterDelegate? _routerDelegate) =>
+      super.noSuchMethod(
+        Invocation.setter(#routerDelegate, _routerDelegate),
+        returnValueForMissingStub: null,
+      );
+
+  @override
+  _i6.GoRouteInformationProvider get routeInformationProvider =>
+      (super.noSuchMethod(
+            Invocation.getter(#routeInformationProvider),
+            returnValue: _FakeGoRouteInformationProvider_6(
+              this,
+              Invocation.getter(#routeInformationProvider),
+            ),
+          )
+          as _i6.GoRouteInformationProvider);
+
+  @override
+  set routeInformationProvider(
+    _i6.GoRouteInformationProvider? _routeInformationProvider,
+  ) => super.noSuchMethod(
+    Invocation.setter(#routeInformationProvider, _routeInformationProvider),
+    returnValueForMissingStub: null,
+  );
+
+  @override
+  _i7.GoRouteInformationParser get routeInformationParser =>
+      (super.noSuchMethod(
+            Invocation.getter(#routeInformationParser),
+            returnValue: _FakeGoRouteInformationParser_7(
+              this,
+              Invocation.getter(#routeInformationParser),
+            ),
+          )
+          as _i7.GoRouteInformationParser);
+
+  @override
+  set routeInformationParser(
+    _i7.GoRouteInformationParser? _routeInformationParser,
+  ) => super.noSuchMethod(
+    Invocation.setter(#routeInformationParser, _routeInformationParser),
+    returnValueForMissingStub: null,
+  );
+
+  @override
+  bool get overridePlatformDefaultLocation =>
+      (super.noSuchMethod(
+            Invocation.getter(#overridePlatformDefaultLocation),
+            returnValue: false,
+          )
+          as bool);
+
+  @override
+  _i8.GoRouterState get state =>
+      (super.noSuchMethod(
+            Invocation.getter(#state),
+            returnValue: _FakeGoRouterState_8(this, Invocation.getter(#state)),
+          )
+          as _i8.GoRouterState);
+
+  @override
+  bool canPop() =>
+      (super.noSuchMethod(Invocation.method(#canPop, []), returnValue: false)
+          as bool);
+
+  @override
+  String namedLocation(
+    String? name, {
+    Map<String, String>? pathParameters = const {},
+    Map<String, dynamic>? queryParameters = const {},
+    String? fragment,
+  }) =>
+      (super.noSuchMethod(
+            Invocation.method(
+              #namedLocation,
+              [name],
+              {
+                #pathParameters: pathParameters,
+                #queryParameters: queryParameters,
+                #fragment: fragment,
+              },
+            ),
+            returnValue: _i14.dummyValue<String>(
+              this,
+              Invocation.method(
+                #namedLocation,
+                [name],
+                {
+                  #pathParameters: pathParameters,
+                  #queryParameters: queryParameters,
+                  #fragment: fragment,
+                },
+              ),
+            ),
+          )
+          as String);
+
+  @override
+  void go(String? location, {Object? extra}) => super.noSuchMethod(
+    Invocation.method(#go, [location], {#extra: extra}),
+    returnValueForMissingStub: null,
+  );
+
+  @override
+  void restore(_i15.RouteMatchList? matchList) => super.noSuchMethod(
+    Invocation.method(#restore, [matchList]),
+    returnValueForMissingStub: null,
+  );
+
+  @override
+  void goNamed(
+    String? name, {
+    Map<String, String>? pathParameters = const {},
+    Map<String, dynamic>? queryParameters = const {},
+    Object? extra,
+    String? fragment,
+  }) => super.noSuchMethod(
+    Invocation.method(
+      #goNamed,
+      [name],
+      {
+        #pathParameters: pathParameters,
+        #queryParameters: queryParameters,
+        #extra: extra,
+        #fragment: fragment,
+      },
+    ),
+    returnValueForMissingStub: null,
+  );
+
+  @override
+  _i10.Future<T?> push<T extends Object?>(String? location, {Object? extra}) =>
+      (super.noSuchMethod(
+            Invocation.method(#push, [location], {#extra: extra}),
+            returnValue: _i10.Future<T?>.value(),
+          )
+          as _i10.Future<T?>);
+
+  @override
+  _i10.Future<T?> pushNamed<T extends Object?>(
+    String? name, {
+    Map<String, String>? pathParameters = const {},
+    Map<String, dynamic>? queryParameters = const {},
+    Object? extra,
+  }) =>
+      (super.noSuchMethod(
+            Invocation.method(
+              #pushNamed,
+              [name],
+              {
+                #pathParameters: pathParameters,
+                #queryParameters: queryParameters,
+                #extra: extra,
+              },
+            ),
+            returnValue: _i10.Future<T?>.value(),
+          )
+          as _i10.Future<T?>);
+
+  @override
+  _i10.Future<T?> pushReplacement<T extends Object?>(
+    String? location, {
+    Object? extra,
+  }) =>
+      (super.noSuchMethod(
+            Invocation.method(#pushReplacement, [location], {#extra: extra}),
+            returnValue: _i10.Future<T?>.value(),
+          )
+          as _i10.Future<T?>);
+
+  @override
+  _i10.Future<T?> pushReplacementNamed<T extends Object?>(
+    String? name, {
+    Map<String, String>? pathParameters = const {},
+    Map<String, dynamic>? queryParameters = const {},
+    Object? extra,
+  }) =>
+      (super.noSuchMethod(
+            Invocation.method(
+              #pushReplacementNamed,
+              [name],
+              {
+                #pathParameters: pathParameters,
+                #queryParameters: queryParameters,
+                #extra: extra,
+              },
+            ),
+            returnValue: _i10.Future<T?>.value(),
+          )
+          as _i10.Future<T?>);
+
+  @override
+  _i10.Future<T?> replace<T>(String? location, {Object? extra}) =>
+      (super.noSuchMethod(
+            Invocation.method(#replace, [location], {#extra: extra}),
+            returnValue: _i10.Future<T?>.value(),
+          )
+          as _i10.Future<T?>);
+
+  @override
+  _i10.Future<T?> replaceNamed<T>(
+    String? name, {
+    Map<String, String>? pathParameters = const {},
+    Map<String, dynamic>? queryParameters = const {},
+    Object? extra,
+  }) =>
+      (super.noSuchMethod(
+            Invocation.method(
+              #replaceNamed,
+              [name],
+              {
+                #pathParameters: pathParameters,
+                #queryParameters: queryParameters,
+                #extra: extra,
+              },
+            ),
+            returnValue: _i10.Future<T?>.value(),
+          )
+          as _i10.Future<T?>);
+
+  @override
+  void pop<T extends Object?>([T? result]) => super.noSuchMethod(
+    Invocation.method(#pop, [result]),
+    returnValueForMissingStub: null,
+  );
+
+  @override
+  void refresh() => super.noSuchMethod(
+    Invocation.method(#refresh, []),
+    returnValueForMissingStub: null,
+  );
+
+  @override
+  void dispose() => super.noSuchMethod(
+    Invocation.method(#dispose, []),
+    returnValueForMissingStub: null,
+  );
 }

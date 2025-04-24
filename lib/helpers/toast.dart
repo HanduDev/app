3a4cdffff82a51aa5cd638c414c2c71e26548f32
matessarini <mariana.tessarini@sou.unifeob.edu.br>
2025-04-24 20:45:31 -0300

    HND-49 alteração do design dos toasts

--- lib/helpers/toast.dart
+++ lib/helpers/toast.dart
@@ -5,9 +5,17 @@ abstract class Toast {
   static void error(BuildContext context, String message) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(message, style: Font.primary()),
+        content: Row(
+          children: [
+            Icon(Icons.error_outline, color: Colors.white),
+            SizedBox(width: 8),
+            Expanded(child: Text(message, style: Font.primary())),
+          ],
+        ),
         backgroundColor: Colors.red,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+        margin: const EdgeInsets.all(32.0),
+        behavior: SnackBarBehavior.floating,
       ),
     );
   }
@@ -15,11 +23,22 @@ abstract class Toast {
   static void success(BuildContext context, String message) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(
-          message,
-          style: Font.primary(fontWeight: FontWeight.w600),
+        content: Row(
+          children: [
+            Icon(Icons.check_circle_outline, color: Colors.white),
+            SizedBox(width: 8),
+            Expanded(
+              child: Text(
+                message,
+                style: Font.primary(fontWeight: FontWeight.w600),
+              ),
+            ),
+          ],
         ),
         backgroundColor: const Color.fromARGB(255, 76, 176, 80),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+        margin: const EdgeInsets.all(32.0),
+        behavior: SnackBarBehavior.floating,
       ),
     );
   }
@@ -27,8 +46,17 @@ abstract class Toast {
   static void info(BuildContext context, String message) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(message, style: Font.primary()),
+        content: Row(
+          children: [
+            Icon(Icons.info_outline, color: Colors.white),
+            SizedBox(width: 8),
+            Expanded(child: Text(message, style: Font.primary())),
+          ],
+        ),
         backgroundColor: const Color.fromARGB(255, 129, 198, 255),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+        margin: const EdgeInsets.all(32.0),
+        behavior: SnackBarBehavior.floating,
       ),
     );
   }

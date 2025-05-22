a9a354056472eae1f84648ccd834191665586bc7
LucasPizol <lucaspizolfe@gmail.com>
2025-05-22 15:50:05 -0300

    HND-72 Corrige enviroment

--- lib/config/environment.dart
+++ lib/config/environment.dart
@@ -1,5 +1,5 @@
 abstract class Environment {
-  static String baseUrl = 'http://172.22.64.111:8080/api/v1';
+  static String baseUrl = 'http://54.233.43.189:8080/api/v1';
   static String googleClientId =
       '1082803010557-v27ef3hee3qm45klpjrj4koeovqb02eu.apps.googleusercontent.com';
 }
